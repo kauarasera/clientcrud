529adfd4014a44d7fe9a9aef7de7067abb8d6cdd
Kauã Rasera <kauarasera@gmail.com>
2025-08-03 08:28:38 +0100

    Start project - renamed Application to Main

---
+++ src/test/java/com/kauarasera/clientcrud/ClientcrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kauarasera.clientcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientcrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
