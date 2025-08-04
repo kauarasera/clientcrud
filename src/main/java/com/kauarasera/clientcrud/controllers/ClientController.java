3b86ae0570ae829c99fe0e7f8f0a136bdb08a726
Kauã Rasera <kauarasera@gmail.com>
2025-08-04 10:55:08 +0100

    Customize HTTP responses using ResponseEntity for better control

--- src/main/java/com/kauarasera/clientcrud/controllers/ClientController.java
+++ src/main/java/com/kauarasera/clientcrud/controllers/ClientController.java
@@ -6,12 +6,16 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping(value = "/clients")
@@ -21,20 +25,22 @@ public class ClientController {
     private ClientService clientService;
 
     @GetMapping("/{id}")
-    public ClientDTO findById(@PathVariable Long id) {
+    public ResponseEntity<ClientDTO> findById(@PathVariable Long id) {
         ClientDTO dto = clientService.findById(id);
-        return dto;
+        return ResponseEntity.ok(dto);
     }
 
     @GetMapping
-    public Page<ClientDTO> findAll(Pageable pageable) {
+    public ResponseEntity<Page<ClientDTO>> findAll(Pageable pageable) {
         Page<ClientDTO> dto = clientService.findAll(pageable);
-        return dto;
+        return ResponseEntity.ok(dto);
     }
 
     @PostMapping
-    public ClientDTO insert(@RequestBody @Valid ClientDTO dto) {
+    public ResponseEntity<ClientDTO> insert(@RequestBody @Valid ClientDTO dto) {
         dto = clientService.insert(dto);
-        return dto;
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
     }
 }
